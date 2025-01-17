a6cf6e7189ea0a135d83678a7b9be6b6c3ad72f0
Aditya Kumar <100521099+adityaK486@users.noreply.github.com>
2025-01-17 10:40:40 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'https://leaderboard-backend-swart.vercel.app/',
+  baseURL: 'https://leaderboard-backend-swart.vercel.app/api/users',
  
 });
 
